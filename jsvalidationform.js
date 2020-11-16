e7a6aa763df3fd041373c18d9bebb5d06275afad
Sanusi Hamed <66240392+shoptsc@users.noreply.github.com>
2020-11-16 13:38:04 +0100

    Update jsvalidationform.js

--- jsvalidationform.js
+++ jsvalidationform.js
@@ -4,59 +4,59 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkInput();
 })
 
-function checkInput(){
+function checkInput() {
     // get values from the inputs
     const usernameValue = username.value.trim();
     const emailValues = email.value.trim();
     const passwordValues = password.value.trim();
     const password2Values = password2.value.trim();
 
-    if (usernameValue === ''){
+    if (usernameValue === '') {
         //show error
         //add error class
         setErrorFor(username, 'Username cannot be blank')
-      }else{
+    } else {
         // add success class
         setSuccessFor(username);
-      }
+    }
 
-    if (emailValues === ''){
+    if (emailValues === '') {
         //show error
         //add error class
         setErrorFor(email, 'email cannot be blank')
-      }else{
+    } else {
         // add success class
         setSuccessFor(email);
-      }
+    }
 
-     if (password === ''){
+    if (passwordValues === '') {
         //show error
         //add error class
         setErrorFor(password, 'password cannot be blank')
-      }else{
+    } else {
         // add success class
         setSuccessFor(password);
-      }
+    }
 
-       if (password2 === ''){
+    if (password2Values === '') {
         //show error
         //add error class
         setErrorFor(password2, 'password2 cannot be blank')
-      }else if(passwordValues !== password2Values){
-            setErrorFor(password2, 'Password does not match');
-      }else{
+    } else if (passwordValues !== password2Values) {
+        setErrorFor(password2, 'Password does not match');
+    } else {
         // add success class
         setSuccessFor(password2);
-      }
+    }
 }
 
-function setErrorFor(input, message){
+function setErrorFor(input, message) {
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
 
@@ -67,37 +67,7 @@ function setErrorFor(input, message){
     formControl.className = 'form-control error'
 }
 
-function setSuccessFor(input){
+function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
